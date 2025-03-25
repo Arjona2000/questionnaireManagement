f48afa98c0d90f8bd418b41cceb5da7a3a7a17ef
Arjona2000 <alejandroarjona07@gmail.com>
2025-03-25 17:18:31 +0100

    Añadida clase principal de la aplicación

---
+++ src/main/java/com/interview/questionnaireManagement/QuestionnaireManagementApplication.java
@@ -0,0 +1,13 @@
+package com.interview.questionnaireManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireManagementApplication.class, args);
+	}
+
+}
